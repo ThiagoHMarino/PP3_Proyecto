31587a9dedacef356f204aba78da4f13ecce832c
RenZSU372 <renzocazzulino@gmail.com>
2025-10-07 23:21:44 -0300

    Menu: cabecera y .cpp

---
+++ menu.h
@@ -0,0 +1,17 @@
+//menu.h
+
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+#include <string>
+#include <ncurses.h>
+
+//**********************  definicion de funciones modulares para el menu *************************/
+
+void funcion_menu();
+void show_message_box();
+void draw_menu();
+
+//************************************************************************************************/
+#endif // MENU_H

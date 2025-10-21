93d1011a3f60eab40f810695bccb7221f697a067
ThiagoHMarino <thiagomarino@MacBook-Air-de-Thiago.local>
2025-10-21 19:02:20 -0300

    Armado de database.cpp y database.h

---
+++ database.cpp
@@ -0,0 +1,23 @@
+#include "database.h"
+#include <sqlite3.h>
+#include <string>
+#include <iostream>
+
+DataBase :: DataBase(string nBD): datab(nullptr), nombreBD(nBD) {
+    int retorno = sqlite3_open(nombreBD.c_str(), &datab);
+
+    if(retorno!=SQLITE_OK){ //SQLITE_OK: operacion de exito.
+        //hubo error:
+        cout << "Error al abrir la BD: " << sqlite3_errmsg(datab) << endl; //sqlite3_errmsg(datab): mensaje de error.
+        datab = nullptr;
+    }else{
+        cout << "BASE DE DATOS: " << nombreBD << " abierta con éxito!" << endl;
+    }
+}
+
+DataBase :: ~DataBase(){
+    if (datab!=nullptr) {
+        sqlite3_close(datab);
+        cout << "BD cerrada." << endl;
+    }
+}
---
+++ database.h
@@ -0,0 +1,47 @@
+#ifndef PP3_PROYECTO_DATABASE_H
+#define PP3_PROYECTO_DATABASE_H
+
+#include "Archivo.h"
+#include <sqlite3.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class DataBase{
+private:
+    sqlite3 * datab; //puntero a una bd de sqlite
+    string nombreBD;
+public:
+    DataBase(string nBD);
+    ~DataBase();
+
+    void crearTablas(); // Metodo que crea todas las tablas
+    bool tablasExisten(); // Para verificar si ya existen
+
+    // Métodos para Vehiculos
+    bool guardarVehiculo(Vehiculo* vehiculo);
+    bool guardarMoto(Moto* moto);
+    bool guardarAuto(Auto* automovil);
+    vector<Vehiculo*> cargarVehiculos();
+    vector<Vehiculo*> cargarVehiculosDisponibles();
+    bool actualizarDisponibilidadVehiculo(string patente, bool disponible);
+
+    // Métodos para Clientes
+    bool guardarCliente(Cliente cliente);
+    vector<Cliente> cargarClientes();
+    Cliente* buscarClientePorDNI(int dni);
+
+    // Métodos para Contratos
+    bool guardarContrato(Contrato contrato);
+    vector<Contrato> cargarContratos();
+    vector<Contrato> cargarContratosActivos();
+    bool finalizarContrato(int id_contrato, float costo_final);
+
+    // Métodos auxiliares
+    Vehiculo* crearVehiculoDesdeBD(string tipo, string marca, string patente,
+    int anio, float precio_base, bool disponible,int cilindradas, int puertas);
+
+};
+
+#endif //PP3_PROYECTO_DATABASE_H
